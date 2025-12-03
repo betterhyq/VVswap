2dee798120e37d6fad60b16c34a7c5e9b8161404
heyongqi10 <heyongqi10@jd.com>
2025-12-03 22:59:35 +0800

    feat: 组织页面滑动

--- src/layout.tsx
+++ src/layout.tsx
@@ -6,11 +6,11 @@ export default function Layout() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <main className="flex flex-1 flex-col gap-4 p-4 pt-0 overflow-hidden h-screen">
         <div className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
         </div>
-        <div className="flex-1 space-y-4 p-6 pt-0">
+        <div className="flex-1 space-y-4 p-6 pt-0 overflow-y-auto">
           <Outlet />
         </div>
       </main>
